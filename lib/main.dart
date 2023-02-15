5e228d0019352ef3e81b18085f68c6d30cf20a71
Ramy Bouchareb <rami.bouchareb@etu.usthb.dz>
2023-02-15 20:24:25 +0100

    state managment + win condition

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+enum PlayerState {
+  playerX,
+  playerY,
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,109 +12,147 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const GameBoard(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class GameBoard extends StatefulWidget {
+  const GameBoard({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GameBoard> createState() => _GameBoardState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GameBoardState extends State<GameBoard> {
+  static PlayerState player = PlayerState.playerX;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<String> boardState = ["", "", "", "", "", "", "", "", "", ""];
+  late bool gameEnded;
+  bool draw = false;
+  @override
+  void initState() {
+    super.initState();
+    gameEnded = false;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: [_headerText(), _board(), _resetbutton()],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _headerText() {
+    if (!gameEnded) {
+      return Text('Player ${player == PlayerState.playerX ? "X" : "Y"} Turn');
+    } else if (!draw) {
+      return Text('Player ${player == PlayerState.playerX ? "X" : "Y"} Won !');
+    } else {
+      return const Text("Draw !!");
+    }
+  }
+
+  Widget _board() {
+    return Align(
+      alignment: Alignment.center,
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height / 2,
+        child: GridView.builder(
+          itemCount: 9,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+          itemBuilder: (context, index) {
+            return _box(index);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _box(int index) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          if (boardState[index] == "" && !gameEnded) {
+            boardState[index] = player == PlayerState.playerX ? "X" : "Y";
+            hasWon();
+            if (!gameEnded) {
+              player = player == PlayerState.playerX
+                  ? PlayerState.playerY
+                  : PlayerState.playerX;
+            }
+          }
+        });
+      },
+      child: Container(
+        color: const Color.fromARGB(255, 84, 84, 84),
+        margin: const EdgeInsets.all(5),
+        child: Center(
+          child: Text(
+            boardState[index],
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 50,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _resetbutton() {
+    return FloatingActionButton(
+      child: const Icon(Icons.replay),
+      onPressed: () {
+        setState(() {
+          boardState = ["", "", "", "", "", "", "", "", "", ""];
+          gameEnded = false;
+          draw = false;
+          player = PlayerState.playerX;
+        });
+      },
+    );
+  }
+
+  void hasWon() {
+    if (boardState.every((e) => e != "")) {
+      gameEnded = true;
+      draw = true;
+    }
+
+    List<List<int>> winningPos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (var element in winningPos) {
+      String pos1 = boardState[element[0]];
+      String pos2 = boardState[element[1]];
+      String pos3 = boardState[element[2]];
+      if (pos1 != "" && pos2 != "" && pos3 != "") {
+        if (pos1 == pos2 && pos2 == pos3) {
+          gameEnded = true;
+        }
+      }
+    }
   }
 }
